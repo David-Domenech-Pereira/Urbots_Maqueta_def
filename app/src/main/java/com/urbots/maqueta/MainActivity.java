141d980b4b878d1e0c33225bb6741d0027439f79
Pere Roca <pere.roca@estudiants.urv.cat>
2023-06-26 18:03:00 +0200

    Tenim toolbar chavales :)

--- app/src/main/java/com/urbots/maqueta/MainActivity.java
+++ app/src/main/java/com/urbots/maqueta/MainActivity.java
@@ -4,12 +4,20 @@ package com.urbots.maqueta;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.ComponentActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.widget.Toast;
 
 import com.urbots.maqueta.cdu.ConfigurarMaqueta;
 import com.urbots.maqueta.cdu.InteractuarCiutat;
@@ -17,19 +25,22 @@ import com.urbots.maqueta.cdu.InteractuarEolica;
 import com.urbots.maqueta.cdu.InteractuarNuclear;
 import com.urbots.maqueta.cdu.InteractuarPanells;
 
-public class MainActivity extends ComponentActivity {
+public class MainActivity extends AppCompatActivity {
+    boolean esClar = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final RelativeLayout relativeLayout = findViewById(R.id.fonsBotons);
         setContentView(R.layout.main_activity);
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         Button boton_eolica = findViewById(R.id.InteractuarEolica);
         Button boton_solar = findViewById(R.id.InteractuarPanells);
         Button boton_ciutat = findViewById(R.id.InteractuarCiutat);
         Button boton_nuclear = findViewById(R.id.InteractuarNuclear);
         ImageButton boton_conf = findViewById(R.id.ConfigurarMaqueta);
-        Button boton_fons = findViewById(R.id.Fons);
-        final RelativeLayout relativeLayout = findViewById(R.id.fonsBotons);
-        final ImageView logo = findViewById(R.id.imageView);
+
+
         boton_eolica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,24 +81,29 @@ public class MainActivity extends ComponentActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
-        boton_fons.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Button b = (Button)view;
-                String text = b.getText().toString();
-                if (text.equalsIgnoreCase("Fons fosc")) {
-                    boton_fons.setText("Fons clar");
-                    relativeLayout.setBackgroundResource(R.color.dark_grey);
-                    logo.setBackgroundResource(R.color.dark_grey);
-                } else {
-                    boton_fons.setText("Fons fosc");
-                    relativeLayout.setBackgroundResource(R.color.white);
-                    logo.setBackgroundResource(R.color.white);
-                }
-
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        final RelativeLayout relativeLayout = findViewById(R.id.fonsBotons);
+         if (id == R.id.opcio1) {
+             if (esClar) {
+                 relativeLayout.setBackgroundResource(R.color.dark_grey);
+                 Toast.makeText(this, "Fons fosc aplicat", Toast.LENGTH_SHORT).show();
+                 esClar = false;
+             } else {
+                 relativeLayout.setBackgroundResource(R.color.white);
+                 Toast.makeText(this, "Fons clar aplicat", Toast.LENGTH_SHORT).show();
+                 esClar = true;
+             }
+         }
+        return true;
     }
 
 }
